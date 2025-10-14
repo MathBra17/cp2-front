936176e6a3d2817597f0e0d678ab1f3632074ac6
Matheus Brasil <matheusbrasilangelotti@gmail.com>
2025-10-14 20:58:20 -0300

    Exercício 3 Concluído

--- src/components/Ex3.jsx
+++ src/components/Ex3.jsx
@@ -6,7 +6,12 @@ export default function Ex3() {
   return (
     <div className="my-8">
         <h1 className="text-2xl font-bold mb-4">Ex3 - Grid Simples</h1>
-      {/* Resolva aqui */}
+      <div Class="grid grid-cols-2 grid-rows-2 h-80 w-60 gap-4">
+        <div Class=" bg-gray-300"></div>
+        <div Class=" bg-gray-300"></div>
+        <div Class=" bg-gray-300"></div>
+        <div Class=" bg-gray-300"></div>
+      </div>
     </div>
   );
 }
