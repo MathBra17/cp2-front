c2d7b6b7fb553059d31e6bd30dbcd562dd2f9ec9
Matheus Brasil <matheusbrasilangelotti@gmail.com>
2025-10-14 20:28:06 -0300

    Exercício 1 Concluído

--- src/components/Ex1.jsx
+++ src/components/Ex1.jsx
@@ -5,8 +5,10 @@
 export default function Ex1() {
   return (
     <div className="my-8">
-        <h1 className="text-2xl font-bold mb-4">Ex1 - Botão Estilizado</h1>
-      {/* Resolva aqui */}
+      <h1 className="text-2xl font-bold mb-4">Ex1 - Botão Estilizado</h1>
+      <div class="bg-blue-600 border-r-0 rounded-md  p-1.5 w-14 hover:bg-blue-700">
+        <button class="text-white">Botão</button>
+      </div>
     </div>
   );
 }
